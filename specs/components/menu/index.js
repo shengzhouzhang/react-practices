b2389815aef333d57e80c805875fc80b93b2bfeb
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-02-20 21:00:15 +1100

    check MenuItem

--- specs/components/menu/index.js
+++ specs/components/menu/index.js
@@ -16,6 +16,7 @@ describe('Menu Item Component', () => {
   it("should render title and items", () => {
     let thisComponent = shallow(<Menu title="TEST_TITLE" items={TEST_ITEMS} />);
     expect(thisComponent.contains(<div className="header">TEST_TITLE</div>));
+    expect(thisComponent.find(MenuItem)).to.have.length(TEST_ITEMS.length);
     expect(thisComponent.contains(<MenuItem name="TEST_NAME_1" imageUrl="TEST_IMAGE_URL_1" />));
     expect(thisComponent.contains(<MenuItem name="TEST_NAME_2" imageUrl="TEST_IMAGE_URL_2" />));
   });
