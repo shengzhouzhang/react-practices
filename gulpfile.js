bf90a7842f32cfedaf85a878fae10069d47ebc05
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-02-22 11:39:00 +1100

    add gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 
 var gulp = require("gulp");
 var babel = require('gulp-babel');
+var rimraf = require('gulp-rimraf');
 
-gulp.task('babelify', function (options, a, b) {
-  return gulp.src(['src/**/*.js'])
-    .pipe(babel())
-    .pipe(gulp.dest('build'));
+gulp.task('clean', function() {
+ return gulp.src([ 'src/**/*.js', 'specs/**/*.js' ], { read: false })
+   .pipe(rimraf({ force: true }));
 });
