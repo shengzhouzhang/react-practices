9f6b53a70960bcc939047abab1cf9d430580049e
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-02-24 14:34:34 +1100

    remove common repositories

--- src/browser/index.tsx
+++ src/browser/index.tsx
@@ -2,9 +2,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import APP_ROUTES from '../routes';
-import { Agent } from '../repositories/Agent';
-import { PhotosRepository } from '../repositories/Photos';
-import { parse } from '../browser/repositories/photos';
+import { Agent } from '../browser/repositories/Agent';
+import { PhotosRepository } from '../browser/repositories/Photos';
 import { GirdContainer } from '../components/PhotoGrid/Container';
 
 const BASE_URI = '';
@@ -14,7 +13,7 @@ let agent = new Agent(BASE_URI);
 function renderMenuView () {
   let photos = JSON.parse(document.querySelector('.initial-state').innerHTML);
   ReactDOM.render(
-    <GirdContainer photos={photos} repository={new PhotosRepository(agent, APP_ROUTES.PhotoAPI, parse)} />,
+    <GirdContainer photos={photos} repository={new PhotosRepository(agent, APP_ROUTES.PhotoAPI)} />,
     document.querySelector('.app-container')
   );
 };
--- src/browser/repositories/Photos.ts
+++ src/browser/repositories/Photos.ts
@@ -2,12 +2,32 @@
 import * as _ from 'lodash';
 import * as Promise from 'bluebird';
 import { IPhotos, IPhoto } from '../../domains/photo';
+import { IAgent } from '../../browser/repositories/Agent';
 
-export function parse (entity: any = {}): IPhotos {
-  return {
-    title: entity.title,
-    items: _.map(entity.items, (item: any): IPhoto => {
-      return { name: item.name, imageUrl: item.imageUrl };
-    })
+export interface IPhotosRepository {
+  fetchPhotos(): Promise<IPhotos>;
+};
+
+export class PhotosRepository implements IPhotosRepository {
+  agent: IAgent;
+  path: string;
+
+  constructor (agent: IAgent, path: string) {
+    this.agent = agent;
+    this.path = path;
   };
+
+  public fetchPhotos = (): Promise<IPhotos> => {
+    return this.agent.request('GET', this.path)
+      .then(entity => this.parse(entity));
+  };
+
+  private parse (entity: any = {}): IPhotos {
+    return {
+      title: entity.title,
+      items: _.map(entity.items, (item: any): IPhoto => {
+        return { name: item.name, imageUrl: item.imageUrl };
+      })
+    };
+  }
 };
