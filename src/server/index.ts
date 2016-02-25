da2e9b1f09a22e348a502f9ec81d2d6833facb81
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-02-25 19:52:12 +1100

    add default controller

--- src/server/index.ts
+++ src/server/index.ts
@@ -10,7 +10,8 @@ import APP_CONFIG from './config';
 import APP_ROUTES from '../routes';
 import { Agent } from '../server/repositories/Agent';
 import { PhotosRepository } from '../server/repositories/Photos-1';
-import PhotoAppController from '../server/controllers/apps/photos';
+import PhotoAppController from '../server/controllers/apps/Photos';
+import DefaultController from '../server/controllers/default';
 import logger from '../server/utils/logger';
 
 let agent = new Agent('https://api.flickr.com/services/rest/?', APP_CONFIG.FLICKR_KEY);
@@ -26,9 +27,6 @@ server.set('views', path.join(__dirname, 'templates'));
 server.use(bodyParser.json());
 
 server.use(APP_ROUTES.PhotoAPP, new PhotoAppController(photosRepository).route);
+server.use('*', new DefaultController(APP_ROUTES.PhotoAPP).route);
 
 export default server;
-
-// server.listen(APP_CONFIG.PORT, function () {
-//   logger.info('Server on %s', APP_CONFIG.PORT);
-// });
