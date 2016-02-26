55300a8ea76e41626f199c10b1f551d8aead4807
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-02-26 20:40:27 +1100

    use webpack instead webpack stream

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@
 var gulp = require("gulp");
 var rimraf = require('gulp-rimraf');
 var sass = require('gulp-sass');
-var webpack = require('webpack-stream');
 var ts = require('gulp-typescript');
+var gutil = require("gulp-util");
+var webpack = require('webpack');
 var runSequence = require('run-sequence');
 
 gulp.task('clean', function() {
@@ -23,12 +24,20 @@ gulp.task('sass', function () {
 });
 
 gulp.task("webpack", function (callback) {
-  return gulp.src('./dist/src/browser/index.js')
-  .pipe(webpack({
-    devtool: '#source-map',
-    output: { filename: "bundle.js" }
-  }))
-  .pipe(gulp.dest('dist/assets'));
+  webpack({
+    entry: [ './dist/src/browser/index.js' ],
+    evtool: '#source-map',
+    output: {
+      path: './dist/assets',
+      filename: "bundle.js"
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({ minimize: true })
+    ]
+  }, function(err, stats) {
+    if(err) throw new gutil.PluginError("webpack", err);
+    callback();
+  });
 });
 
 gulp.task('typescript', function () {
