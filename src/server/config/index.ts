775dd994600ef957afb1f059f8eac546c8ba86bc
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-02-26 19:55:12 +1100

    remove default seatch tag

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -1,13 +1,11 @@
 
 const CONFIG = {
   PORT: parseInt(process.env.PORT),
-  DEFAULT_SEARCH: process.env.DEFAULT_SEARCH,
   FLICKR_KEY: process.env.FLICKR_KEY,
   TZ: 'Australia/Sydney'
 };
 
 console.info('PORT', CONFIG.PORT);
-console.info('DEFAULT_SEARCH', CONFIG.DEFAULT_SEARCH);
 console.info('FLICKR_KEY', CONFIG.FLICKR_KEY);
 
 export default CONFIG;
